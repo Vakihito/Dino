0a75aacb8df97325c2ca70a4f60f127631c70637
Victor Akihito Kamada Tomita <40372914+Vakihito@users.noreply.github.com>
2019-08-10 01:02:14 -0300

    modularização

--- The_Dino/teste/t1.py
+++ The_Dino/teste/t1.py
@@ -6,9 +6,37 @@ CONTS_YINI = 300
 CONTS_MAX_HIGHT = 285
 CONTS_GRAV = 0.5
 
+#verifica se apertou o botao de fechar o programa
+def check_if_quit():
+    for event in pygame.event.get():    #list the events and checks if it's 
+        if event.type == pygame.QUIT:
+            return False
+    return True
+
+#verifica os dados durante o pulo
+def on_jump(char_y, vel_y):
+    #verifica se esta no ar
+    if char_y < CONTS_MAX_HIGHT : 
+        if keys[pygame.K_DOWN]:
+            print("entrei aqui1")
+            return vel_y + 0.5
+        return vel_y + 0.25
+    return vel_y
+
+# reseta as variaveis de posicao em relacao a y se estiver a baixo do solo
+# verifica se esta apertando para ir para baixo se estiver abaixo do solo
+# e abaixa o boneco
+def reset(char_y, vel_y, char_hight):
+
+    if char_y >= CONTS_YINI:
+        if keys[pygame.K_DOWN]:
+            return(CONTS_YINI + 10, 0, 20)
+        return (CONTS_YINI, 0, 30)
+    return (char_y, vel_y, char_hight)
+
+
+
 pygame.init() #inicializando jogo
-
-
 
 win = pygame.display.set_mode((1600,600))
 pygame.display.set_caption("first Game")
@@ -27,52 +55,25 @@ tempo_ini = 0
 
 run = True
 
-def check_if_quit():
-    for event in pygame.event.get():    #list the events and checks if it's 
-        if event.type == pygame.QUIT:
-            return False
-    return True
-
-
-    
-
 while run:
     pygame.time.delay(10) #delay da imagem
     
-    # for event in pygame.event.get():    #list the events and checks if it's 
-    #     if event.type == pygame.QUIT:
-    #         run = False
     run = check_if_quit()
-    keys = pygame.key.get_pressed()#tecla selecionada
 
+    keys = pygame.key.get_pressed()#tecla selecionada
 
     #e verifica se esta apertando para cima verifica se 250 <= char_y <= 300, espaco de pulo
     if keys[pygame.K_UP] and char_y <= CONTS_YINI and char_y >= CONTS_MAX_HIGHT:
         vel_y -= 2
    
     #verifica se esta no pulo                                  
-    if char_y < CONTS_MAX_HIGHT :
-        vel_y += 0.25
-        if keys[pygame.K_DOWN]:
-            vel_y += 0.5
+    vel_y = on_jump(char_y, vel_y)
 
     #atualiza a posicao de acordo com a velocidade
     char_y += vel_y
 
-    if char_y >= 300:
-        char_y = 300
-        vel_y = 0
-        char_hight = 30
-        char_y = CONTS_YINI
-
-        if keys[pygame.K_DOWN]:
-            char_hight = 20
-            char_y = CONTS_YINI + 10
-
-
-
     
-    
+    (char_y, vel_y, char_hight) = reset(char_y, vel_y, char_hight)
     win.fill((255,255,255)) #fills with wight
    
     # print(char_y)
--- The_Dino/teste/test.py
+++ The_Dino/teste/test.py
@@ -1,7 +1,12 @@
 
-x = 15
 
-def change(*x):
-    x = 10
 
-print(x)
+def change():
+    if x == 15:
+        print("sou goiaba")
+
+def main():
+    x = 15
+    change()
+    print(x)
+main()
